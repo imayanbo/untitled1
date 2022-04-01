5b6a1d03ed6a4b166eedc7e4efb201f540cb3279
Layne <Layne@qq.com>
2022-04-01 12:56:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author mayanbo
+ * @date 1/4/2022 - 下午12:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
